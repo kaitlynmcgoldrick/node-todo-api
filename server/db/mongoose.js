c12b04a0c6bb576a22927894fa26e2e613017f36
Kaitlyn McGoldrick <mcgoldrick.kaitlyn@gmail.com>
2018-04-04 13:59:09 -0400

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+// mongoose.connect("mongodb://localhost:27017/TodoApp");
 
 module.exports = {
   mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ app.get('/todos/:id', (req, res) => {
 }); 
 })
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findOneAndRemove({_id: id}, (err, result) => {
+    if (!result) {
+      return res.status(404).send();
+    }
+    res.status(200).send(result);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up on port ${port}`);
 });
